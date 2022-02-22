c8e4e940ab6d6d663c212f3771588fd284b8a5f9
Muhammad Atiq <atiqm@sheridancollege.ca>
2022-02-22 13:10:14 -0500

    added .gitignore and TestUserProfile class

--- src/midtermexam_versiona_extensioncode/TestUserProfile.java
+++ src/midtermexam_versiona_extensioncode/TestUserProfile.java
@@ -13,10 +13,22 @@ public class TestUserProfile {
         Scanner input = new Scanner(System.in);
         String id;
         int genre;
+        String genreIn = "";
         System.out.println("Enter the User ID you wish to register with.");
         id = input.nextLine();
         System.out.println("Please select yout favorite genre of movies:\n0: Comedy\n1: Drama\n2: Action\n3: Mystery");
         genre = input.nextInt();
+        switch(genre){
+            case 0:
+                genreIn = "Comedy";
+            case 1:
+                genreIn = "Drama";
+            case 2:
+                genreIn = "Action";
+            case 3:
+                genreIn = "Mystery";
+        }
+        UserProfile user = new UserProfile(id, genreIn);
         System.out.println("Profile created!");
     }
 }
